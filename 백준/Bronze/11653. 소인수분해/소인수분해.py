eb0a39ddb865c584d9a18e014e7ed3c027e2c518
jeonguking <136977963+jeonguking@users.noreply.github.com>
2025-02-02 21:11:11 +0700

    [Bronze I] Title: 소인수분해, Time: 1020 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+
+while True:
+    if n == 1 :
+        break
+    for i in range(2,n+1):
+        if n%i == 0 :
+            print(i)
+            n = n//i
+            break
